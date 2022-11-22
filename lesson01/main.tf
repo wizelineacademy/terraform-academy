509a5770ae3885a5ee6d735f652b06c7c76262be
Adolfo Del Castillo Gomez <adolfo.delcastillo@Adolfos-MacBook-Pro-2.local>
2022-11-22 12:12:49 -0600

    Added change about commends lines and removed ineccesary files

--- lesson01/main.tf
+++ lesson01/main.tf
@@ -2,13 +2,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-# If you prefer, you can store the Terraform state in S3
-# https://www.terraform.io/docs/backends/types/s3.html
-# terraform {
-#   backend "s3" {
-#     region = "us-east-1"
-#   }
-# }
-
-# Add below your code
--- lesson01/output.tf
+++ lesson01/output.tf
@@ -1,18 +1,3 @@
-# Uncomment this when you have the solution
-
-# output "web_ip" {
-#   value = aws_instance.web.public_ip
-# }
-
-# output "web_dns" {
-#   value = aws_instance.web.public_dns
-# }
-
-# Only uncomment if you have a hosted zone in Route53
-# output "web_dns" {
-#   value = aws_route53_record.dns_web.name
-# }
-
 output "dns_publica_servidor1" {
   description = "DNS public server"
   value = "http://${aws_instance.myServer.public_dns}:${var.server_port}"
